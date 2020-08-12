6fe4fb980699a19277b523a37eabd6bf22476982
wattana thongsonkleeb <wpsc2642@gmail.com>
2020-08-12 09:52:43 +0700

    part5debug2

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,39 +1,18 @@
-/*import React from 'react';
+import React from 'react'
 import CharacterCard from './CharacterCard';
-
 export default function WordCard(props){
+    
 
-   const activationHandler = c => { 
-      console.log(`${c} has been activated.`)
-   }
- return (
- <div>
-    { 
-    Array.from(props.value).map((c, i) =>
-    <CharacterCard value={c} key={i} activationHandler={activationHandler}/>
-    }
- </div>
- );
-}*/
-
-import React, { Component } from 'react';
-import CharacterCard from './CharacterCard';
-
-export default class WordCard extends Component {
-
-    activationHandler = c => {
-        console.log(${c} has been activated.) 
+    const activationHandler = c =>{
+        console.log(`${c} has been activated`)
     }
 
-    render() {
-        return (
-            <div>
-                { 
-                    Array.from(this.props.value).map((c, i) => 
-                    <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>)
-                
-                }
-            </div>
-        );
-    }
+    return (
+        <div>
+            {
+                Array.from(props.value).map((c, i) => 
+                <CharacterCard value={c} key={i} activationHandler = {activationHandler}/>)
+            }
+        </div>
+    );
 }
